0e4b49fffbb7a6596d0499abd73242c52067a132
JeremieG06300 <jeremie.garod@epitech.eu>
2023-02-07 14:07:12 +0100

    fist commit

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
+
+const About = () => {
+  return (
+    <div>
+        <Logo/>
+        <Navigation/>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Vitae suscipit
+        tellus mauris a diam maecenas sed enim. Vulputate mi sit amet mauris
+        commodo quis. Eu ultrices vitae auctor eu augue ut lectus. In egestas
+        erat imperdiet sed euismod nisi porta lorem. Id porta nibh venenatis
+        cras sed felis eget velit. Fermentum et sollicitudin ac orci phasellus
+        egestas tellus rutrum tellus. Et malesuada fames ac turpis egestas sed
+        tempus urna. Tempus urna et pharetra pharetra massa massa ultricies mi.
+        A erat nam at lectus urna duis convallis convallis.
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae
+          suscipit tellus mauris a diam maecenas sed enim. Vulputate mi sit amet
+          mauris commodo quis. Eu ultrices vitae auctor eu augue ut lectus. In
+          egestas erat imperdiet sed euismod nisi porta lorem. Id porta nibh
+          venenatis cras sed felis eget velit. Fermentum et sollicitudin ac orci
+          phasellus egestas tellus rutrum tellus. Et malesuada fames ac turpis
+          egestas sed tempus urna. Tempus urna et pharetra pharetra massa massa
+          ultricies mi. A erat nam at lectus urna duis convallis convallis.
+        </p>
+      </p>
+    </div>
+  )
+}
+
+export default About
